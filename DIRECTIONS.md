5976699e176f82ab2b29c1351ba075305494f782
sreneewatkins <74524591+sreneewatkins@users.noreply.github.com>
2021-03-05 11:26:49 -0600

    Initial Commit

---
+++ DIRECTIONS.md
@@ -0,0 +1,14 @@
+
+
+We now have a fully functional app! Remember that this is for Atlanta's 
+transit system and we hardcoded "GA" into our API request, so it's only 
+valid to test locations in Georgia. Here are a few:
+
+Address: 65 Alabama St SW
+City: Atlanta
+
+Address: 1761 Howell Mill Rd NW
+City: Atlanta
+
+Address: 215 College Cir
+City: Buchanan
---
+++ src/main/resources/static/custom.js
@@ -0,0 +1,20 @@
+/*
+This code centers the map on the closest bus and then for each bus, drops a
+pin at the bus' location.
+*/
+
+var map;
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: parseFloat(busLocations[0].LATITUDE), lng: parseFloat(busLocations[0].LONGITUDE) },
+        zoom: 15,
+        scrollwheel: false
+    });
+
+    for (i=0; i<busLocations.length; i++){
+        var marker = new google.maps.Marker({
+            position: { lat: parseFloat(busLocations[i].LATITUDE), lng: parseFloat(busLocations[i].LONGITUDE) },
+            map: map,
+        });
+    }
+}
