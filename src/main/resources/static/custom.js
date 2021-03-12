4170c0aba2c60cd043fc486198d2fac49d308d5b
sreneewatkins <74524591+sreneewatkins@users.noreply.github.com>
2021-03-12 16:26:30 -0600

    add bus image and bus number to info window

--- src/main/resources/static/custom.js
+++ src/main/resources/static/custom.js
@@ -12,12 +12,45 @@ function initMap() {
     });
 
     for (i=0; i<busLocations.length; i++){
-        var marker = new google.maps.Marker({
+        let marker = new google.maps.Marker({
             position: {
                 lat: parseFloat(busLocations[i].LATITUDE),
                 lng: parseFloat(busLocations[i].LONGITUDE)
             },
             map: map,
+            icon: {
+                url: "/bluebus.png",
+                scaledSize: new google.maps.Size(50, 50)
+            },
+            animation: google.maps.Animation.BOUNCE,
         });
-    }
-}
+
+        let busNum = busLocations[i].VEHICLE;
+        let contentString = '<h4> Bus#:' + busNum + '</h4>';
+        let infowindow = new google.maps.InfoWindow({
+            content: contentString
+        })
+        google.maps.event.addListener(marker, 'click', () => infowindow.open(map, marker));
+
+    }//end for
+
+//    let busNum = busLocations[i].VEHICLE;
+//    let contentString = '<h2> Bus#:' + busNum + ' will print here</h2>';
+
+
+    //may need to delete this down to line 40
+    /*
+    const transitLayer = new google.maps.TransitLayer();
+          transitLayer.setMap(map);
+        const trafficLayer = new google.maps.TrafficLayer();
+          trafficLayer.setMap(map);
+
+            let contentString = '<h2> Bus#:' + busNum + ' will print here</h2>';
+
+          let infowindow = new google.maps.InfoWindow({
+                content: contentString
+            })
+        google.maps.event.addListener(marker, 'click', () => infowindow.open(map, marker));
+    */
+
+}//end initMap()
